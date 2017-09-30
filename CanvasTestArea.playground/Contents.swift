67a573bbe31168b7613b255341b7bd730fccfea1
Romeyo Caines <rcaines@lcs-mba3264.lcs.on.ca>
2017-09-30 11:57:49 -0400

    done some of the rays

--- CanvasTestArea.playground/Contents.swift
+++ CanvasTestArea.playground/Contents.swift
@@ -21,18 +21,22 @@ import Cocoa
 import PlaygroundSupport
 
 // Create a new canvas
-let canvas = Canvas(width: 300, height: 500)
-
-// Draw a circle at the origin with radius of 50 pixels
-canvas.drawEllipse(centreX: 0, centreY: 0, width: 50, height: 50)
-
-// Show where the origin is
-canvas.drawAxes()
-
-// Draw an ellipse in a different color at the centre of the canvas
-canvas.fillColor = Color.green
-canvas.drawEllipse(centreX: 150, centreY: 250, width: 50, height: 100)
-//draw rectangle to the top of the screen
-canvas.drawRectangle(centreX: 150, centreY: 400, width: 100, height: 50)
+let canvas = Canvas(width: 500, height: 500)
+canvas.fillColor = Color.init(hue: 200, saturation: 50, brightness: 100, alpha: 100)
+canvas.drawRectangle(bottomLeftX: 0, bottomLeftY: 0, width: 500, height: 500)
+canvas.lineColor = Color.yellow
+canvas.drawLine(fromX: 0, fromY: 500, toX: 0, toY: 0, lineWidth: 8)
+canvas.drawLine(fromX: 0, fromY: 500, toX: 105, toY: 0, lineWidth: 8)
+canvas.drawLine(fromX: 0, fromY: 500, toX: 230, toY: 0, lineWidth: 8)
+canvas.drawLine(fromX: 0, fromY: 500, toX: 370, toY: 0, lineWidth: 8)
+canvas.drawLine(fromX: 0, fromY: 500, toX: 500, toY: 0, lineWidth: 8)
+canvas.drawLine(fromX: 0, fromY: 500, toX: 700, toY: 0, lineWidth: 8)
+canvas.drawLine(fromX: 0, fromY: 500, toX: 1050, toY: 0, lineWidth: 8)
+canvas.drawLine(fromX: 0, fromY: 500, toX: 1900, toY: 0, lineWidth: 8)
+canvas.drawLine(fromX: 0, fromY: 500, toX: 4000, toY: 0, lineWidth: 8)
+canvas.drawLine(fromX: 0, fromY: 500, toX: 55000, toY: 0, lineWidth: 8)
+canvas.lineColor = Color.orange
+canvas.drawLine(fromX: 0, fromY: 500, toX: 50, toY: 0 , lineWidth: 8)
+canvas.drawLine(fromX: 0, fromY: 500, toX: 170, toY: 0, lineWidth: 8)
 // This code is necessary to see the result in the Assistant Editor at right
 PlaygroundPage.current.liveView = canvas.imageView
